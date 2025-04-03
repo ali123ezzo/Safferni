b01e8a0956b0d48537a19b9307c0168cb244abd5
ali123ezzo <ali123ezzo@gmail.com>
2025-04-03 10:57:37 +0300

    making an app for user authentications to make project clear

--- Bus_Company/Bus_Company/settings.py
+++ Bus_Company/Bus_Company/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'BusCompany',
+    'UserAuth',
     'rest_framework',
     'rest_framework.authtoken',
 ]
